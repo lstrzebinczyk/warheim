315a7948af3627c8f132c3dab3d84ea18ba3d8f3
Łukasz Strzebinczyk <lukasz.strzebinczyk@gmail.com>
2021-02-14 13:28:15 +0100

    wip

--- lib/warheim/rules/band/goblin_pirates.ex
+++ lib/warheim/rules/band/goblin_pirates.ex
@@ -3,7 +3,8 @@ defmodule Warheim.Rules.Band.GoblinPirates do
     %{
       id: "goblin_pirates",
       name: "Załoga Zielonoskórych Kaprów",
-      alignment_id: "evil"
+      alignment_id: "evil",
+      special_rules: ["tax", "wasteful"]
     }
   end
 end
---
+++ lib/warheim/rules/special_rule.ex
@@ -0,0 +1,29 @@
+defmodule Warheim.Rules.SpecialRule do
+  def all do
+    [
+      %{
+        id: "tax",
+        name: "Podatek",
+        description: """
+          Być może kompania służy organizacji, która pośredniczy w zakupie ekwipunku,
+          a może Gildia Kupców nałożyła na *drużynę* dodatkowy podatek. Niezależnie od przyczyny,
+          dla kompanii oznacza to dodatkowe koszty związane z pozyskaniem najpospolitszych nawet przedmiotów.
+          W grze odzwierciedla to zasada, zgodnie z którą *drużyna* za każdy zakupiony w trakcie *sekwencji po potycznce*
+          przedmiot muszą zapłacić dodatkowym złotem. I tak, wartość przedmiotu, którego cena nie przekracza **20** ZK wzrasta
+          o kolejne **K6** ZK; ekwipunek, którego wartość podstawowa oscyluje w granicach od **21** ZK do **50** ZK zostaje
+          zwiększona o kolejne **2K6** ZK, zaś przedmioty, których cena podstawowa przekracza **51** ZK muszą dopłacić kolejne **3K6** ZK.
+        """
+      },
+      %{
+        id: "wasteful",
+        name: "Rozrzutność",
+        description: """
+          Kompanie podlegające *zasadzie specjalnej* `Rozrzutność` trwonią spore nakłady finansowe na zapewnienie odpowiedniego wiktu i opierunku,
+          lub w celu ukrycia swej prawiwej natury. W grze odzwierciedla to zasada, zgodnie z którą w trakcie *sekwencji po potyczce*, na potrzeby
+          zasad sprzedaży *łupów* kompania traktowana jest tak, jakby licza modeli w *drużynie* była o jeden poziom większa,
+          niż to faktycznie ma miejsce.
+        """
+      }
+    ]
+  end
+end
