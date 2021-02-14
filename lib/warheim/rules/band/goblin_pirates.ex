7677e1f2d18e7a02e7c40e82e90367dfc34f086a
Łukasz Strzebinczyk <lukasz.strzebinczyk@gmail.com>
2021-02-14 15:19:31 +0100

    wip

--- lib/warheim/rules/band/goblin_pirates.ex
+++ lib/warheim/rules/band/goblin_pirates.ex
@@ -5,7 +5,35 @@ defmodule Warheim.Rules.Band.GoblinPirates do
       name: "Załoga Zielonoskórych Kaprów",
       alignment_id: "evil",
       special_rule_ids: ["tax", "wasteful"],
-      band_size: 3..15
+      band_size: 3..15,
+      unit_templates: [
+        captain_template()
+      ]
+    }
+  end
+
+  defp captain_template do
+    %{
+      name: "Kapitan Goblinów",
+      race: "goblin",
+      unit_type: "hero",
+      cost: 50,
+      count: 1..1,
+      availability: nil,
+      base: "20x20",
+      initial_chars: %{
+        movement: 4,
+        weapon_skill: 4,
+        balistic_skill: 4,
+        strength: 3,
+        toughness: 3,
+        wounds: 2,
+        initiative: 4,
+        attacks: 1,
+        leadership: 7
+      },
+      inventory: ["knife", "pistol"],
+      special_rules: ["leader", "they_smell_weird"]
     }
   end
 end
--- lib/warheim/rules/race.ex
+++ lib/warheim/rules/race.ex
@@ -1,16 +1,27 @@
 defmodule Warheim.Rules.Race do
-  defmodule Warheim.Rules.Race.Dwarf do
+  defmodule Warheim.Rules.Race.Goblin do
     def data do
       %{
-        id: "dwarf",
-        name: "Krasnolud"
+        id: "goblin",
+        name: "Goblin",
+        max_chars: %{
+          movement: 5,
+          weapon_skill: 5,
+          balistic_skill: 6,
+          strength: 3,
+          toughness: 4,
+          wounds: 3,
+          initiative: 6,
+          attacks: 4,
+          leadership: 7
+        }
       }
     end
   end
 
   def all do
     [
-      Warheim.Rules.Race.Dwarf.data()
+      Warheim.Rules.Race.Goblin.data()
     ]
   end
 end
