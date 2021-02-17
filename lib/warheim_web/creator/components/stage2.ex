d19815a0be28710ca99e5e3b55e77816ef9d94e6
Łukasz Strzebinczyk <lukasz.strzebinczyk@gmail.com>
2021-02-17 19:09:43 +0100

    move more components to surface

--- lib/warheim_web/creator/components/stage2.ex
+++ lib/warheim_web/creator/components/stage2.ex
@@ -1,58 +1,48 @@
 defmodule Creator.Components.Stage2 do
-  use WarheimWeb, :live_component
+  use Surface.LiveComponent
 
-  def update(%{state: state}, socket) do
-    socket =
-      socket
-        |> assign(:state, state)
-        |> assign(:is_new_unit_modal_opened, false)
-        # |> assign(:is_new_unit_modal_opened, true)
-
-    {:ok, socket}
-  end
-
-  def handle_event("open-new-unit-modal", _, %{assigns: %{state: state}} = socket) do
-    socket =
-      socket
-        |> assign(:is_new_unit_modal_opened, true)
-
-    {:noreply, socket}
-  end
-
-  def handle_event("close-new-unit-modal", _, %{assigns: %{state: state}} = socket) do
-    socket =
-      socket
-        |> assign(:is_new_unit_modal_opened, false)
-
-    {:noreply, socket}
-  end
+  prop state, :any
+  data is_new_unit_modal_opened, :boolean, default: false
 
   def render(assigns) do
-    ~L"""
-    <div class="row">
-      <div class="col-4">
-        <%= live_component @socket, Creator.Components.BandDetails, band_type_id: @state.band_type_id %>
-      </div>
-      <div class="col">
-        <button type="button" phx-target="<%= @myself %>" phx-click="open-new-unit-modal">
-          Dodaj jednostkę
-        </button>
-      </div>
-        <%= if @is_new_unit_modal_opened do %>
-          <div class="x-modal-background" phx-target="<%= @myself %>" phx-click="close-new-unit-modal"></div>
-          <div class="x-modal">
-            <div class="x-modal-content">
-              <span class="close" phx-target="<%= @myself %>" phx-click="close-new-unit-modal">&times;</span>
-              <p>
-                <%= for unit_type <- Warheim.Rules.UnitType.all() do %>
-                  <div><%= unit_type.name %></div>
-                <% end %>
-              </p>
+    ~H"""
+      <div class="row">
+        <div class="col-4">
+          <Creator.Components.BandDetails band_type_id={{ @state.band_type_id }} />
+        </div>
+        <div class="col">
+          <button type="button" :on-click="open-new-unit-modal">
+            Dodaj jednostkę
+          </button>
+          <div :if={{ @is_new_unit_modal_opened }}>
+            <div class="x-modal-background" :on-click="close-new-unit-modal"></div>
+            <div class="x-modal">
+              <div class="x-modal-content">
+                <span class="close" :on-click="close-new-unit-modal">&times;</span>
+                <p>
+                  <div :for={{ unit_type <- unit_types() }}>
+                    {{ unit_type.name }}
+                  </div>
+                </p>
+              </div>
             </div>
           </div>
-        <% end %>
+        </div>
       </div>
-    </div>
     """
+  end
+
+  def unit_types do
+    Warheim.Rules.UnitType.all()
+  end
+
+  def handle_event("open-new-unit-modal", _, socket) do
+    socket = assign(socket, :is_new_unit_modal_opened, true)
+    {:noreply, socket}
+  end
+
+  def handle_event("close-new-unit-modal", _, socket) do
+    socket = assign(socket, :is_new_unit_modal_opened, false)
+    {:noreply, socket}
   end
 end
